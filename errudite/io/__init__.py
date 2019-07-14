4ca928ffda98946dd805d9761decacc3552cc803
Tongshuang Wu <wtshuang@cs.uw.edu>
2019-07-13 20:39:53 -0700

    Revert "Debug dataset"

--- errudite/io/__init__.py
+++ errudite/io/__init__.py
@@ -1,5 +1,5 @@
 from .dataset_reader import DatasetReader
 from .sst_reader import SSTReader
-from .snli_reader import SNLIReader
 from .squad_reader import SQUADReader
-from .mrqa_reader import MRQAReader
+from .snli_reader import SNLIReader
+
--- errudite/predictors/qa/mrqa_allennlp/__init__.py
+++
@@ -1,4 +0,0 @@
-from .mrqa_predictor import *
-from .BERT_QA import *
-from .mrqa_iterator import *
-from .mrqa_reader import *
